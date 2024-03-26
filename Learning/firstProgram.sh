420c6f897aa0b83e67d4a6b00202b64e9154497f
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-26 14:50:42 +0600

    changing some files

--- Learning/firstProgram.sh
+++ Learning/firstProgram.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-nmap $1
-dirsearch -u $1 -e php
+echo "Creating directory $1_recon."
+mkdir $1_recon;
+nmap $1 > $1_recon/nmap
+echo "The results of nmap scan are stored in $1_recon/namp"
+dirsearch -u $1 -e php > $1_recon/dirsearch
+echo "The results of dirsearch scan are stored in $1_recon/dirsearch."
 
 
