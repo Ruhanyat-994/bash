a7bc396d030ef7eaac635511d05f42fcb7d4cc34
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-26 23:14:24 +0600

    adding spaces into condition

--- Learning/secondProgram.sh
+++ Learning/secondProgram.sh
@@ -5,15 +5,15 @@ DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 echo "Creating a Directory $DIRECTORY"
 mkdir $DIRECTORY
-if [ $2 == "nmap"]
+if [ $2 == "nmap" ]
 then
         nmap $DOMAIN > $DIRECTORY/nmap
-elif [ $2 == "dirsearch"]
+elif [ $2 == "dirsearch" ]
 then
-        dirsearch -u $DOMAIN -e php > $DIRECTORY/dirse>
+        dirsearch -u $DOMAIN -e php > $DIRECTORY/dirsearch
 else
         nmap $DOMAIN > $DIRECTORY/nmap
-        echo "The results of nmap is stored in $DIRECT>
-        dirsearch -u $DOMAIN -e php > $DIRECTORY/dirse>
-        echo "The results of dirsearch is stored in $D>
+        echo "The results of nmap is stored in $DIRECTORY/nmap"
+        dirsearch -u $DOMAIN -e php > $DIRECTORY/dirsearch
+        echo "The results of dirsearch is stored in $DIRECTORY/dirsearch"
 fi
