5ab6d69402aaee64a3f544a85f78166608531f6e
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-30 23:27:12 +0600

    this is an automated script

--- Learning/fifthProgram.sh
+++ Learning/fifthProgram.sh
@@ -1,63 +1,65 @@
+#!/bin/bash
 
 TODAY=$(date)
 echo "The time for the scan is $TODAY"
 
-DOMAIN=$1
-DIRECTORY=${DOMAIN}_recon
-
-echo "Creating a Directory $DIRECTORY"
-mkdir $DIRECTORY
-
-nmap_scan()
-{
-         nmap $DOMAIN > $DIRECTORY/nmap
-         echo "The results of nmap is stored in $DIRECTORY/nmap"
+nmap_scan() {
+    nmap $DOMAIN > "$DIRECTORY/nmap"
+    echo "The results of nmap are stored in $DIRECTORY/nmap"
 }
 
-dirsearch_scan()
-{
-        dirsearch -u $DOMAIN -e php > $DIRECTORY/dirsearch
-        echo "The results of dirsearch is stored in $DIRECTORY/dirsearch"
-
-}
-crt_scan()
-{
-        curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt
-        echo "The results of crt search in stored in $DIRECTORY/crt"
-
+dirsearch_scan() {
+    dirsearch -u $DOMAIN -e php > "$DIRECTORY/dirsearch"
+    echo "The results of dirsearch are stored in $DIRECTORY/dirsearch"
 }
 
-case $2 in
+crt_scan() {
+    curl "https://crt.sh/?q=$DOMAIN&output=json" -o "$DIRECTORY/crt"
+    echo "The results of crt search are stored in $DIRECTORY/crt"
+}
+
+getopts "m:" OPTION
+MODE=$OPTARG
+
+for i; do
+    DOMAIN=$i
+    DIRECTORY="${DOMAIN}_recon"
+    echo "Creating directory $DIRECTORY"
+    mkdir -p "$DIRECTORY"
+
+    case $MODE in 
         nmap)
-                nmap_scan
-                ;;
+            nmap_scan
+            ;;
         dirsearch)
-                dirsearch_scan
-                ;;
+            dirsearch_scan
+            ;;
         crt)
-                crt_scan
-                ;;
+            crt_scan
+            ;;
         *)
-                nmap_scan
-                dirsearch_scan
-                crt_scan
-                ;;
-esac
+            nmap_scan
+            dirsearch_scan
+            crt_scan
+            ;;
+    esac
 
-echo "+++++++++++++Generating recon report from the output file+++++++++++++"
-TODAY=$(date)
-echo "This scan was created on $TODAY" > $DIRECTORY/report
+    echo "+++++++++++++Generating recon report from the output file+++++++++++++"
+    TODAY=$(date)
+    echo "This scan was created on $TODAY" > "$DIRECTORY/report"
 
-grep -E "^\s*\S+\s+\S+\s+\S+\s*$" $DIRECTORY/nmap >> $DIRECTORY/report
+    if [ -f "$DIRECTORY/nmap" ]; then
+        echo "Results for nmap:" >> "$DIRECTORY/report"
+        grep -E "^\s*\S+\s+\S+\s+\S+\s*$" "$DIRECTORY/nmap" >> "$DIRECTORY/report"
+    fi
 
-echo "The results for dirsearch:" >> $DIRECTORY/report
-echo "Results for crt.sh:" >> $DIRECTORY/report
+    if [ -f "$DIRECTORY/dirsearch" ]; then
+        echo "Results for dirsearch:" >> "$DIRECTORY/report"
+        cat "$DIRECTORY/dirsearch" >> "$DIRECTORY/report"
+    fi
 
-jq -r ".[] | .name_value" $DIRECTORY/crt >>  $DIRECTORY/report
-
-
-
-
-
-
-
+    if [ -f "$DIRECTORY/crt" ]; then
+        echo "Results for crt:" >> "$DIRECTORY/report"
+        jq -r ".[] | .name_value" "$DIRECTORY/crt" >> "$DIRECTORY/report"
+    fi
+done
